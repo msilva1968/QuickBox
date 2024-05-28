654ce3cfd28455ffacb958befbf43f042119935d
Marcio Silva <marcio.silva.ter@estacio.br>
2024-05-28 13:04:44 -0300

    Front-end ajustes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import PaginaBaseAdmin from './paginas/Administracao/PaginaBaseAdmin';
 import Home from './paginas/Home';
+import NotFound from './paginas/NotFound';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path='/admin' element={<PaginaBaseAdmin />}>
       </Route>
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
